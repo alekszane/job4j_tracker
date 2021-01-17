b126bd13e47d9fcd554a7b369a461b06a097e620
Alekszane <alekszane@gmail.com>
2021-01-17 19:28:45 +0300

    Status objects

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -1,4 +1,22 @@
 package ru.job4j.oop;
 
 public class Battery {
+    private  int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery enother) {
+        this.load = this.load - enother.load;
+        enother.load = enother.load + this.load;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(100);
+        Battery second = new Battery(40);
+        first.exchange(second);
+        System.out.println(first.load);
+        System.out.println(second.load);
+    }
 }
