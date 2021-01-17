041bb34e8daabbb4808a80a862b27ecf183097c3
Alekszane <alekszane@gmail.com>
2021-01-17 17:45:46 +0300

    Calling method with return type

---
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+    public static String engToRus(String eng) {
+        return  "Неизвестное слово. " + eng;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyDic.engToRus("Tratata");
+        System.out.println(rsl);
+    }
+}
+
+
