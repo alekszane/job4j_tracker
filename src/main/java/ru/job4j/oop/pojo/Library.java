744b3f9adbe70f024ef100371c4637067df9a8ad
Alekszane <alekszane@gmail.com>
2021-01-22 23:51:13 +0300

    Delete indexOfNull

--- src/main/java/ru/job4j/oop/pojo/Library.java
+++ src/main/java/ru/job4j/oop/pojo/Library.java
@@ -1,16 +1,9 @@
 package ru.job4j.oop.pojo;
 
+import java.util.Arrays;
+
 public class Library {
-    public static int indexOfNull(Book[] books) {
-        int rsl = -1;
-        for (int i = 0; i < books.length; i++) {
-            if (books[i] == null) {
-                rsl =  i;
-                break;
-            }
-        }
-        return rsl;
-    }
+
     public static void main(String[] args) {
         Book java = new Book("Head start Java", 320);
         Book code = new Book("Clean code", 540);
@@ -21,21 +14,16 @@ public class Library {
         books[1] = code;
         books[2] = kotlin;
         books[3] = python;
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
-            System.out.println(book.getName() + " - " + book.getNumberPages() + " pages");
-        }
+        Arrays.stream(books).map(book -> book.getName() + " - " + book.getNumberPages() + " pages").forEach(System.out::println);
         Book temp = books[0];
         books[0] = books[3];
         books[3] = temp;
         System.out.println("Replace null and third elements");
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
+        for (Book book : books) {
             System.out.println(book.getName() + " - " + book.getNumberPages() + " pages");
         }
         System.out.println("Show only Clean code book!");
-        for (int i = 0; i < books.length; i++) {
-            Book book = books[i];
+        for (Book book : books) {
             if (book.getName().equals("Clean code")) {
                 System.out.println(book.getName() + " - " + book.getNumberPages() + " pages");
             }
