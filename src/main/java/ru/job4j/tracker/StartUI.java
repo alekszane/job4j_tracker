666e6fc1be3b51f454315157aff85f1da3fee5a5
Alekszane <alekszane@gmail.com>
2021-01-25 23:32:25 +0300

    Fix tracker delete and replace methods

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,10 +7,9 @@ public class StartUI {
        tracker.add(new Item(3, "item1"));
        tracker.add(new Item(2, "item1"));
        tracker.add(new Item(1, "item2"));
-       System.out.println(tracker.findById(2).getId());
        Item[] it = tracker.findByName("item1");
        for (int i = 0; i < it.length; i ++){
-           System.out.println(i);
+           System.out.println(it[i].getId());
        }
        Item item1 = new Item(5, "Replace Item");
        tracker.replace(1, item1);
