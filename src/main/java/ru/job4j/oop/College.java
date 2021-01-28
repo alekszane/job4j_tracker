39d0855886b1e4d82b8d00429d66a3dc19ab4821
Alekszane <alekszane@gmail.com>
2021-01-29 00:25:08 +0300

    Casting types. fix casting

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -3,7 +3,7 @@ package ru.job4j.oop;
 public class College {
     public static void main(String[] args) {
         Freshman fr = new Freshman();
-        Student st = fr;
-        Object obj = fr;
+        Student st = new Freshman();
+        Object obj = new Freshman();
     }
 }
