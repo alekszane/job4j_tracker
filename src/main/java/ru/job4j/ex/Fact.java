f5d3fd59ddd523de4945c260576aaaf7142d341a
Alekszane <alekszane@gmail.com>
2021-02-08 22:37:17 +0300

    Test IllegalArgumentException. Junit

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -1,12 +1,7 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public static void main(String[] args) {
-    int test = 0;
-        System.out.println(calc(test));
-    }
-
-    public static int calc(int n) {
+        public static int calc(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException("Метод вызывается с некорректными параметрами");
         }
---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenThrowIllegalArgumentException() {
+        int rsl = Fact.calc(0);
+    }
+
+    @Test()
+    public void whenFactIs10() {
+        int rsl = Fact.calc(10);
+        assertThat(rsl, is(3628800));
+    }
+}
